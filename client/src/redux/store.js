9e22684e1e73889bc5d91ecf1f556e940bbafe02
Yash <er.yshsingh@gmail.com>
2024-10-13 19:01:55 +0530

    Revert "todo application using redux - toolkit"
    This reverts commit ad4a53d3562cf129e61cef5b6d9fdc44bf87e4f5.

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore, ConfigureStore} from '@reduxjs/toolkit';
-
-export const store = configureStore({
-    reducer: {},
-    middleware: (getDefaultMiddleware) =>getDefaultMiddleware({
-        serializableCheck:  false,
-    }),
-});
-
